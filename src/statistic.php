2b159e9cb42f40489630b862ba062bed8a35c7d9
Carsten Wallenhauer <115737488+SourcePot@users.noreply.github.com>
2024-06-16 18:29:13 +0200

    Update statistic.php

--- src/statistic.php
+++ src/statistic.php
@@ -37,48 +37,22 @@ class statistic implements \SourcePot\Datapool\Interfaces\Processor{
      *
      * @param array $callingElementSelector Is the selector for the canvas element which called the method 
      * @param string $action Selects the requested process to be run  
-     * @return bool TRUE the requested action exists or FALSE if not
+     *
+     * @return string|bool Return the html-string or TRUE callingElement does not exist
      */
     public function dataProcessor(array $callingElementSelector=array(),string $action='info'){
         $callingElement=$this->oc['SourcePot\Datapool\Foundation\Database']->entryById($callingElementSelector,TRUE);
-        switch($action){
-            case 'run':
-                if (empty($callingElement)){
-                    return TRUE;
-                } else {
-                return $this->processStatistic($callingElement,$testRunOnly=FALSE);
-                }
-                break;
-            case 'test':
-                if (empty($callingElement)){
-                    return TRUE;
-                } else {
-                    return $this->processStatistic($callingElement,$testRunOnly=TRUE);
-                }
-                break;
-            case 'widget':
-                if (empty($callingElement)){
-                    return TRUE;
-                } else {
-                    return $this->getStatisticWidget($callingElement);
-                }
-                break;
-            case 'settings':
-                if (empty($callingElement)){
-                    return TRUE;
-                } else {
-                    return $this->getStatisticSettings($callingElement);
-                }
-                break;
-            case 'info':
-                if (empty($callingElement)){
-                    return TRUE;
-                } else {
-                    return $this->getStatisticInfo($callingElement);
-                }
-                break;
+        if (empty($callingElement)){
+            return TRUE;
+        } else {
+            return match($action){
+                'run'=>$this->processStatistic($callingElement,$testRunOnly=FALSE),
+                'test'=>$this->processStatistic($callingElement,$testRunOnly=TRUE),
+                'widget'=>$this->getStatisticWidget($callingElement),
+                'settings'=>$this->getStatisticSettings($callingElement),
+                'info'=>$this->getStatisticInfo($callingElement),
+            };
         }
-        return FALSE;
     }
 
     private function getStatisticWidget($callingElement){
